4b80f175fdf450a3af9e1f5edb992b692053b017
Sontaya Rungsaard <sontaya16071997@gmail.com>
2023-04-01 21:55:31 +0700

    🔧 chore(.eslintrc): add eslint configuration file 🗑️ chore(app.vue): delete app.vue file 🔧 chore(nuxt.config.ts): add empty modules and css arrays 🔧 chore(package.json): add eslint, typescript, and @antfu/eslint-config dependencies 🎉 feat(index.vue): add index.vue file with a greeting message

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div
+    class="h-[100dvh] flex justify-center items-center"
+    bg="white dark:dark"
+    text="dark dark:white center"
+  >
+    <span
+      class="animate-bounce"
+      text="5xl"
+    >
+      Hello there, <strong text="primary">I'm gson 👋</strong>
+    </span>
+  </div>
+</template>
